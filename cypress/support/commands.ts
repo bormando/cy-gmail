14adae4e0e8da460ad274bf3a2ae41dae332808d
Dmitrii Bormotov <squier7@gmail.com>
2023-02-18 00:22:54 +0300

    Update environment variables' names

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,9 +3,9 @@ import {getToken, checkInbox, Email, parseLinkFromHtml} from 'gmail-getter'
 Cypress.Commands.add('checkInbox', async (recipient: string) => {
   const username = recipient.split('@')[0]
   const token = await getToken(
-    Cypress.env('clientId'),
-    Cypress.env('clientSecret'),
-    Cypress.env('refreshToken')
+    Cypress.env('CLIENT_ID'),
+    Cypress.env('CLIENT_SECRET'),
+    Cypress.env('REFRESH_TOKEN')
   )
   return checkInbox(token, 30000, 2000, `to:${username}`)
 })
